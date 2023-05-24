6651d85b4d18598e3ca1dee683f891ca7a04141c
ParkJinYong <mej1406@gmail.com>
2023-05-24 18:17:08 +0900

    init

---
+++ src/main/java/com/placehub/PlacehubApplication.java
@@ -0,0 +1,13 @@
+package com.placehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/placehub/PlacehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
