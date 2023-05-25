152996bd87ee60865f6d3a752ef2fc0937c4e4b4
깃waimi3169 <waimi3169@naver.com>
2023-05-25 17:31:01 +0900

    Feat: UserController, UserService class add

---
+++ src/main/java/com/placehub/boundedContext/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.placehub.boundedContext.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("usr/user")
+@RequiredArgsConstructor
+public class UserController {
+}
