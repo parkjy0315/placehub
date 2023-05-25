0aaa1a3d1aae37af32bdd11132d36f99222f6557
ParkJinYong <mej1406@gmail.com>
2023-05-25 18:20:00 +0900

    Feat: Place CRUD, test add

---
+++ src/main/java/com/placehub/boundedContext/place/entity/Place.java
@@ -0,0 +1,21 @@
+package com.placehub.boundedContext.place.entity;
+
+import com.placehub.base.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Place extends BaseEntity {
+    private String placeName;
+    private Double xPos;
+    private Double yPos;
+    private String category;
+    private Long likeCount;
+}
