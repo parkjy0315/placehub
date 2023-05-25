c6ef2f4e1d78aea2738a5fcda02f326f6c83479d
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-25 15:50:10 +0900

    Feat: SecurityConfig add, h2DB setting (#1)

---
+++ src/main/java/com/placehub/base/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.placehub.base.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+}
