21dd17d588ec0520bcaa4742fadaecb9eaa465f0
waimi3169 <125889390+waimi3169@users.noreply.github.com>
2023-05-26 10:20:54 +0900

    Rename User -> Member (#11)
    * Feat : add User Class
    
    * Feat: UserRepository add
    
    * Feat: UserController, UserService class add
    
    * Rename: User -> Member
    
    ---------
    
    Co-authored-by: 깃waimi3169 <깃허waimi3169@naver.com>

---
+++ src/main/java/com/placehub/boundedContext/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.placehub.boundedContext.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("usr/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+}
