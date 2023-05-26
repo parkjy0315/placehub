bb0505ace9ccf0a8d505ae711c70f78832c728b7
깃waimi3169 <waimi3169@naver.com>
2023-05-26 10:46:33 +0900

    Feat: finByUsername add

--- src/main/java/com/placehub/boundedContext/member/repository/MemberRepository.java
+++ src/main/java/com/placehub/boundedContext/member/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package com.placehub.boundedContext.member.repository;
 import com.placehub.boundedContext.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 
 }
