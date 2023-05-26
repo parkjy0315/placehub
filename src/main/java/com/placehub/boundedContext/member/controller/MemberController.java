de1ab366feb1dd65031581f8229df9a25286fe24
깃waimi3169 <waimi3169@naver.com>
2023-05-26 17:45:20 +0900

    Feat: JoinForm 수정

--- src/main/java/com/placehub/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/placehub/boundedContext/member/controller/MemberController.java
@@ -2,18 +2,21 @@ package com.placehub.boundedContext.member.controller;
 
 import com.placehub.boundedContext.member.service.MemberService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("usr/member")
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
@@ -27,15 +30,23 @@ public class MemberController {
     public static class JoinForm {
         @NotBlank
         @Size(min = 4, max = 20)
-        private final String username;
+        private final String member_id;
         @NotBlank
         @Size(min = 4, max = 20)
         private final String password;
+        @NotBlank
+        @Email(message = "유효하지 않은 이메일입니다.")
+        private final String email;
+        @Null
+        private final String username;
+        @Null
+        private final String nickname;
     }
 
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
-        memberService.join(joinForm.getUsername(), joinForm.getPassword());
+        memberService.join(joinForm.getMember_id(), joinForm.getPassword());
 
         return "redirect:/";
     }
