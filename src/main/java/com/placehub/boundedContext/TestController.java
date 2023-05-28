63db559903b6b4ddc1dd07baa635aeec992c0286
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-29 01:57:46 +0900

    Feat: local api test(keyword) (#22)

--- src/main/java/com/placehub/boundedContext/TestController.java
+++ src/main/java/com/placehub/boundedContext/TestController.java
@@ -1,7 +1,10 @@
 package com.placehub.boundedContext;
 
+import com.placehub.base.util.LocalApi;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -10,8 +13,18 @@ public class TestController {
         return "test";
     }
 
-    @GetMapping("/layout")
+    @GetMapping("/local_api_test")
+    @ResponseBody
     public String pageTest2() {
-        return "layout";
+        double xLng = 33.450701; // 경도
+        double yLat = 126.570667; // 위도
+        String keyWord = "카페"; // 키워드
+        int page = 1; // 페이지 수
+        int size = 10; // 한 페이지 내 결과 개수
+        int radius = 10000; //
+
+        JSONObject result = LocalApi.KeyWord.getAll(xLng, yLat, keyWord, radius, page, size);
+
+        return result.toJSONString();
     }
 }
