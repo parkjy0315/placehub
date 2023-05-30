d187a799f9f0f18f2490b2c3acad4826d4fac684
jny0 <pjy100402@naver.com>
2023-05-30 17:49:15 +0900

    Feat : 내정보 페이지

--- src/main/java/com/placehub/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/placehub/boundedContext/member/controller/MemberController.java
@@ -15,6 +15,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,4 +71,9 @@ public class MemberController {
         return "usr/member/login";
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/me")
+    public String showMe(Model model) {
+        return "usr/member/me";
+    }
 }
