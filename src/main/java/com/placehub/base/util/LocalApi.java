7f1396d209c450927a99bf6d3afce164e664f21d
ParkJinYong <mej1406@gmail.com>
2023-05-31 16:00:00 +0900

    Feat: place test

--- src/main/java/com/placehub/base/util/LocalApi.java
+++ src/main/java/com/placehub/base/util/LocalApi.java
@@ -115,6 +115,46 @@ public class LocalApi {
 
             return object;
         }
+
+        /**
+         * 키워드로 장소 검색하기
+         * @param x - 중심좌표의 x좌표 혹은 경도값
+         * @param y - 중심좌표의 y좌표 혹은 위도값
+         * @param category 카테고리 - 검색을 원하는 질의어
+         * @param radius 반경 - 중심좌표부터의 반경거리
+         * @param page 결과 페이지 번호 - (최소:1, 최대:45, 기본값:1)
+         * @param size 한 페이지에 보여질 문서의 개수 - (최소:1, 최대:15, 기본값:15)
+         * @return JSONObejct
+         */
+        public static JSONObject getAllRect(String rect, String category, int radius, int page, int size) {
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(API_SERVER_HOST + CATEGORY_URL)
+                    .queryParam("category_group_code", category)
+                    .queryParam("rect", rect)
+                    .queryParam("page", page)
+                    .queryParam("size", size)
+                    .encode(StandardCharsets.UTF_8)
+                    .build();
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.add("Authorization", "KakaoAK " + REST_API_KEY);
+            headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+            headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
+
+            RequestEntity<String> rq = new RequestEntity<>(headers, HttpMethod.GET, uri.toUri());
+            ResponseEntity<String> re = restTemplate.exchange(rq, String.class);
+
+            JSONParser parser = new JSONParser();
+            JSONObject object = null;
+            try {
+                object = (JSONObject) parser.parse(re.getBody());
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
+            return object;
+        }
     }
 
 }
--- src/main/java/com/placehub/base/util/PlaceData.java
+++ src/main/java/com/placehub/base/util/PlaceData.java
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Component
 @RequiredArgsConstructor
 public class PlaceData {
