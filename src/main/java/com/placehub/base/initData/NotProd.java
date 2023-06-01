dc4eeeef447d9621aeecff78ffeec0886e27db79
jny0 <pjy100402@naver.com>
2023-06-01 13:35:03 +0900

    Feat : NotProd에 테스트용 게시글 생성

--- src/main/java/com/placehub/base/initData/NotProd.java
+++ src/main/java/com/placehub/base/initData/NotProd.java
@@ -4,6 +4,8 @@ import com.placehub.boundedContext.member.entity.Member;
 import com.placehub.boundedContext.member.service.MemberService;
 import com.placehub.boundedContext.place.entity.Place;
 import com.placehub.boundedContext.place.service.PlaceService;
+import com.placehub.boundedContext.post.entity.Post;
+import com.placehub.boundedContext.post.service.PostService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +13,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+
 @Configuration
 @Profile({"dev", "test"})
 public class NotProd {
     @Bean
     CommandLineRunner initData(
             MemberService memberService,
-            PlaceService placeService
+            PlaceService placeService,
+            PostService postService
     ) {
         return new CommandLineRunner() {
             @Override
@@ -35,6 +40,9 @@ public class NotProd {
                         "place", "02-123-1234", "서울 ",
                         12.0, 123.0);
 
+                //Post post1 =
+                        postService.createPost(memberJinyeongKakao.getId(), place.getId(),
+                        "테스트용 게시물입니다.", true, LocalDate.now());
             }
         };
     }
