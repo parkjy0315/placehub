f73119389ec90ede3f07743fc18a4c11a16217fc
ParkJinYong <mej1406@gmail.com>
2023-06-01 17:05:38 +0900

    Fix: category add

--- src/main/java/com/placehub/boundedContext/category/entity/SmallCategory.java
+++ src/main/java/com/placehub/boundedContext/category/entity/SmallCategory.java
@@ -1,6 +1,7 @@
 package com.placehub.boundedContext.category.entity;
 
 import com.placehub.base.entity.BaseEntity;
+import com.placehub.base.entity.Category;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
@@ -8,12 +9,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Calendar;
+
 @SuperBuilder(toBuilder = true)
 @Getter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class SmallCategory extends BaseEntity {
+public class SmallCategory extends Category {
     @Column(unique = true)
     private String categoryName;
 }
