592f64fd38d966075a17a2b2083468156e740f63
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-06-01 14:30:57 +0900

    Revert "Feat: place func, db setting (#40)"
    This reverts commit f3c1cbed8cdc81ae09cbc22aeafc162252336b70.

--- src/main/java/com/placehub/base/util/PlaceData.java
+++ src/main/java/com/placehub/base/util/PlaceData.java
@@ -1,7 +1,6 @@
 package com.placehub.base.util;
 
 
-import com.placehub.base.entity.Category;
 import com.placehub.boundedContext.category.entity.BigCategory;
 import com.placehub.boundedContext.category.entity.MidCategory;
 import com.placehub.boundedContext.category.entity.SmallCategory;
@@ -15,6 +14,7 @@ import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -30,6 +30,30 @@ public class PlaceData {
 
     @Transactional
     public void savePlace(JSONObject placeData) {
+        /*
+        "total_count"
+        "is_end"
+        "pageable_count"
+        "same_name"
+         */
+        JSONObject meta = (JSONObject) placeData.get("meta");
+
+        /*
+        # JSONObject 내용
+
+        "place_url" : "http://place.map.kakao.com/1496650030",
+        "place_name" : "남영동스테이크골목",
+        "category_group_name" : "관광명소",
+        "road_address_name" : "",
+        "category_name" : "여행 > 관광,명소 > 테마거리 > 먹자골목",
+        "distance" : "1449",
+        "phone" : "",
+        "category_group_code" : "AT4",
+        "x" : "126.973236353706",
+        "y" : "37.5439359573409",
+        "address_name" : "서울 용산구 남영동 2",
+        "id" : "1496650030"
+         */
         JSONArray documents = (JSONArray) placeData.get("documents");
         for (int i = 0; i < documents.size(); i++) {
             JSONObject element = (JSONObject) documents.get(i);
@@ -41,63 +65,42 @@ public class PlaceData {
             Double xPos = Double.parseDouble((String) element.get("x"));
             Double yPos = Double.parseDouble((String) element.get("y"));
 
-            Category[] categories = categoryFilter(categoryName);
+            String[] categorySplit = new String[3];
+            String[] temp = categoryName.split(" > ");
+            for (int j = 0; j < Math.min(temp.length, 3); j++) {
+                categorySplit[j] = temp[j];
+            }
 
+            BigCategory bigCategory = null;
+            MidCategory midCategory = null;
+            SmallCategory smallCategory = null;
+            switch (categorySplit.length) {
+                case 3:
+                    smallCategory = smallCategoryService.findByCategoryName(categorySplit[2]);
+                case 2:
+                    midCategory = midCategoryService.findByCategoryName(categorySplit[1]);
+                case 1:
+                    bigCategory = bigCategoryService.findByCategoryName(categorySplit[0]);
+                    break;
+            }
+
+            if (bigCategory == null) {
+                bigCategory = bigCategoryService.create(categorySplit[0]);
+            }
+            if (midCategory == null) {
+                midCategory = midCategoryService.create(categorySplit[1]);
+            }
+            if (smallCategory == null) {
+                smallCategory = smallCategoryService.create(categorySplit[2]);
+            }
 
             placeService.create(
-                    categories[0].getId(),
-                    categories[1].getId(),
-                    categories[2].getId(),
+                    bigCategory.getId(),
+                    midCategory.getId(),
+                    smallCategory.getId(),
                     placeName, phone, addressName,
                     xPos, yPos
             );
         }
     }
-
-    public Category[] categoryFilter(String categoryStr) {
-        String[] categorySplit = new String[3];
-        String[] temp = categoryStr.split(" > ");
-        for (int j = 0; j < Math.min(temp.length, 3); j++) {
-            categorySplit[j] = temp[j];
-        }
-
-        BigCategory bigCategory = null;
-        MidCategory midCategory = null;
-        SmallCategory smallCategory = null;
-        switch (categorySplit.length) {
-            case 3:
-                smallCategory = smallCategoryService.findByCategoryName(categorySplit[2]);
-            case 2:
-                midCategory = midCategoryService.findByCategoryName(categorySplit[1]);
-            case 1:
-                bigCategory = bigCategoryService.findByCategoryName(categorySplit[0]);
-                break;
-        }
-
-        if (bigCategory == null) {
-            bigCategory = bigCategoryService.create(categorySplit[0]);
-        }
-        if (midCategory == null) {
-            midCategory = midCategoryService.create(categorySplit[1]);
-        }
-        if (smallCategory == null) {
-            smallCategory = smallCategoryService.create(categorySplit[2]);
-        }
-
-        return new Category[]{bigCategory, midCategory, smallCategory};
-    }
-
-    public boolean isLastPage(JSONObject placeData) {
-        /*
-        "total_count"
-        "is_end"
-        "pageable_count"
-        "same_name"
-         */
-        JSONObject meta = (JSONObject) placeData.get("meta");
-        boolean isEnd = (boolean) meta.get("is_end");
-        return isEnd;
-    }
-
-
 }
