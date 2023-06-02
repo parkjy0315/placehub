c5bb52ebd535407cbf54eab155227f174558c62b
ParkJinYong <mej1406@gmail.com>
2023-06-02 11:13:29 +0900

    Fix: category id bug fix, placeId add

--- src/main/java/com/placehub/base/entity/Category.java
+++ src/main/java/com/placehub/base/entity/Category.java
@@ -2,14 +2,12 @@ package com.placehub.base.entity;
 
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder(toBuilder = true)
 @Getter
-@Entity
 @NoArgsConstructor
 public class Category extends BaseEntity {
 }
--- src/main/java/com/placehub/boundedContext/place/service/PlaceService.java
+++ src/main/java/com/placehub/boundedContext/place/service/PlaceService.java
@@ -5,6 +5,7 @@ import com.placehub.boundedContext.place.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -13,12 +14,13 @@ public class PlaceService {
     private final PlaceRepository placeRepository;
 
     public Place create(Long bigCategoryId, Long midCategoryId, Long smallCategoryId,
-                        String placeName, String phone, String addressName,
+                        Long placeId, String placeName, String phone, String addressName,
                         Double xPos, Double yPos) {
         Place place = Place.builder()
                 .bigCategoryId(bigCategoryId)
                 .midCategoryId(midCategoryId)
                 .smallCategoryId(smallCategoryId)
+                .placeId(placeId)
                 .placeName(placeName)
                 .phone(phone)
                 .addressName(addressName)
@@ -30,6 +32,11 @@ public class PlaceService {
 
     public Place read(Long id) {
         Optional<Place> place = placeRepository.findById(id);
+        return place.orElse(null);
+    }
+
+    public Place findByPlaceId(Long placeId) {
+        Optional<Place> place = placeRepository.findByPlaceId(placeId);
         return place.orElse(null);
     }
 
