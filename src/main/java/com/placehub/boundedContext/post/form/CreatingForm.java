97ac585be3210d856446ac8ff30246f8c3b9ae31
jny0 <pjy100402@naver.com>
2023-06-13 13:35:32 +0900

    Feat : 게시글 수정 후 해당 게시글로 리다이렉트

--- src/main/java/com/placehub/boundedContext/post/form/CreatingForm.java
+++ src/main/java/com/placehub/boundedContext/post/form/CreatingForm.java
@@ -2,8 +2,10 @@ package com.placehub.boundedContext.post.form;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class CreatingForm extends BasePostFormFromFrontToBack {
     @NotBlank
     public String isOpenToPublic;
