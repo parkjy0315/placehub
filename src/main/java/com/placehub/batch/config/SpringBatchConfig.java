cc30be59a2e09d4e3bec56e76d13ca148c356c50
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-06-14 16:40:59 +0900

    Feat: init batch 추가 (#117)
    * Fix: batch time fix, search category pick fix
    
    * Feat: search page likeCount sort
    
    * Feat: search refactoring(1)
    
    * Feat: search refactoring(2)
    
    * Feat: search refactoring(3)
    
    * Fix: search page category empty remove
    
    * Feat: init batch update

--- src/main/java/com/placehub/batch/config/SpringBatchConfig.java
+++ src/main/java/com/placehub/batch/config/SpringBatchConfig.java
@@ -24,45 +24,45 @@ public class SpringBatchConfig {
     private final JobLauncher jobLauncher;
     private final Job job1;
 
-//    @Scheduled(fixedDelay = 5000)
-//    public void initBatch() {
-//        List<String> categoryCode = new ArrayList<>() {{
-//            add("AT4"); // 1111
-//            add("CT1"); // 2522
-//            add("CE7"); // 33501
-//            add("FD6"); // 135651
-//        }};
-//
-//        double yDist = placeData.getYdist();
-//        double criteria = 0.005;
-//
-//        categoryCode.stream()
-//                .forEach(code -> {
-//                    IntStream.range(0, (int) (yDist / criteria) + 1)
-//                            .boxed()
-//                            .forEach(i -> {
-//                                JobParameters jobParameters = new JobParametersBuilder()
-//                                        .addString("jobID", String.valueOf(System.currentTimeMillis()))
-//                                        .addString("categoryCode", code)
-//                                        .addDouble("criteria", criteria)
-//                                        .addDouble("i", (double) i)
-//                                        .toJobParameters();
-//                                try {
-//                                    jobLauncher.run(job1, jobParameters);
-//                                } catch (JobExecutionAlreadyRunningException e) {
-//                                    throw new RuntimeException(e);
-//                                } catch (JobRestartException e) {
-//                                    throw new RuntimeException(e);
-//                                } catch (JobInstanceAlreadyCompleteException e) {
-//                                    throw new RuntimeException(e);
-//                                } catch (JobParametersInvalidException e) {
-//                                    throw new RuntimeException(e);
-//                                }
-//                            });
-//                });
-//    }
+    @Scheduled(fixedDelay = 5000)
+    public void initBatch() {
+        List<String> categoryCode = new ArrayList<>() {{
+            add("AT4"); // 1111
+            add("CT1"); // 2522
+            add("CE7"); // 33501
+            add("FD6"); // 135651
+        }};
 
-    @Scheduled(cron = "0 0 3 * * MON,WED,FRI")
+        double yDist = placeProcessor.getYdist();
+        double criteria = 0.005;
+
+        categoryCode.stream()
+                .forEach(code -> {
+                    IntStream.range(0, (int) (yDist / criteria) + 1)
+                            .boxed()
+                            .forEach(i -> {
+                                JobParameters jobParameters = new JobParametersBuilder()
+                                        .addString("jobID", String.valueOf(System.currentTimeMillis()))
+                                        .addString("categoryCode", code)
+                                        .addDouble("criteria", criteria)
+                                        .addDouble("i", (double) i)
+                                        .toJobParameters();
+                                try {
+                                    jobLauncher.run(job1, jobParameters);
+                                } catch (JobExecutionAlreadyRunningException e) {
+                                    throw new RuntimeException(e);
+                                } catch (JobRestartException e) {
+                                    throw new RuntimeException(e);
+                                } catch (JobInstanceAlreadyCompleteException e) {
+                                    throw new RuntimeException(e);
+                                } catch (JobParametersInvalidException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                });
+    }
+
+    @Scheduled(cron = "0 0 3 * * MON-FRI")
     public void placeDataSaveBatch() {
         List<String> categoryCode = new ArrayList<>() {{
             add("AT4"); // 1111
