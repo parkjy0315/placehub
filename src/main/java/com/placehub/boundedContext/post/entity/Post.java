7f07348a9945a994b91663aa9a30d00f75768f38
kgpksh <tkdgns543@gmail.com>
2023-06-15 10:16:49 +0900

    Feat : post와 imgaes테이블에 인덱스 추가

--- src/main/java/com/placehub/boundedContext/post/entity/Post.java
+++ src/main/java/com/placehub/boundedContext/post/entity/Post.java
@@ -3,6 +3,8 @@ package com.placehub.boundedContext.post.entity;
 import com.placehub.base.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -17,6 +19,7 @@ import java.time.LocalDate;
 @Getter
 @ToString
 @Where(clause = "delete_date is null")
+@Table(indexes = {@Index(name = "place_index",columnList = "place")})
 public class Post extends BaseEntity implements Comparable<Post> {
     private long member;
     private long place;
